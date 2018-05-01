f28f2dfd7b2936797689294eba39d0c2a9916891
Vladimir Cirkovic <vladimir.cirkovic@relax-gaming.com>
2018-05-01 20:20:33 +0200

    Styling with bootstrap until something starts to make sense

--- src/Logo.jsx
+++ src/Logo.jsx
@@ -1,18 +1,9 @@
 import React from "react";
 import { TMDB_API } from "./constants";
-import styled from "styled-components";
-
-const Logo = styled.div`
-  display: block;
-  width: 100%;
-  max-width: 140px;
-`;
 
 const TMDBLogo = () => (
   <div>
-    <Logo>
-      <img src={TMDB_API.LOGO} alt="The Movie Database" />
-    </Logo>
+    <img src={TMDB_API.LOGO} alt="The Movie Database" />
   </div>
 );
 
--- src/Search.jsx
+++ src/Search.jsx
@@ -5,13 +5,14 @@ import { TMDB_API } from "./constants";
 import styled from "styled-components";
 
 const SearchWrapper = styled.div`
-  margin: 5% 30% 5% 30%;
   font-family: "Fjalla One";
   font-size: 1.2em;
-  width: 100%;
+  width: 50%;
+  height: 50%;
+  float: right;
+  margin-left: 2%;
+  margin-top: 2%;
 `;
-
-const AutocompleteWrapper = styled.div``;
 
 class Search extends React.Component {
   // component state
@@ -22,57 +23,53 @@ class Search extends React.Component {
 
   render() {
     return (
-      <div>
-        <SearchWrapper>
-          <TMDBLogo />
-          <AutocompleteWrapper>
-            <Autocomplete
-              className="Search-autosuggest"
-              inputProps={{
-                id: "movies-autocomplete",
-                placeholder: "Search movie title"
-              }}
-              wrapperStyle={{ position: "relative", display: "inline-block" }}
-              value={this.state.value}
-              items={this.state.suggestions}
-              getItemValue={item => item.title}
-              onSelect={value => {
-                this.setState({ value, suggestions: [] });
-              }}
-              onChange={(event, value) => {
-                this.setState({ value });
-                fetch(
-                  `${TMDB_API.TMDB_URL}/search/movie?query=${value}&api_key=${
-                    process.env.REACT_APP_TMDB_API_KEY
-                  }`
-                )
-                  .then(res => res.json())
-                  .then(data => {
-                    const titles = [];
-                    data.results.map(movie =>
-                      titles.push({
-                        title: movie.original_title,
-                        id: movie.id
-                      })
-                    );
-                    this.setState({ suggestions: titles });
+      <div className="col-xs-12 search-container nopadding">
+        <TMDBLogo />
+        <Autocomplete
+          className="Search-autosuggest"
+          inputProps={{
+            id: "movies-autocomplete",
+            placeholder: "Search movie title"
+          }}
+          wrapperStyle={{ position: "relative", display: "inline-block" }}
+          value={this.state.value}
+          items={this.state.suggestions}
+          getItemValue={item => item.title}
+          onSelect={value => {
+            this.setState({ value, suggestions: [] });
+          }}
+          onChange={(event, value) => {
+            this.setState({ value });
+            fetch(
+              `${TMDB_API.TMDB_URL}/search/movie?query=${value}&api_key=${
+                process.env.REACT_APP_TMDB_API_KEY
+              }`
+            )
+              .then(res => res.json())
+              .then(data => {
+                const titles = [];
+                data.results.map(movie =>
+                  titles.push({
+                    title: movie.original_title,
+                    id: movie.id
                   })
-                  .catch(e => {
-                    console.error(e);
-                    this.setState({ suggestions: [] });
-                  });
-              }}
-              renderItem={(item, isHighlighted) => (
-                <div
-                  className={`item ${isHighlighted ? "item-highlighted" : ""}`}
-                  key={item.id}
-                >
-                  {item.title}
-                </div>
-              )}
-            />
-          </AutocompleteWrapper>
-        </SearchWrapper>
+                );
+                this.setState({ suggestions: titles });
+              })
+              .catch(e => {
+                console.error(e);
+                this.setState({ suggestions: [] });
+              });
+          }}
+          renderItem={(item, isHighlighted) => (
+            <div
+              className={`item ${isHighlighted ? "item-highlighted" : ""}`}
+              key={item.id}
+            >
+              {item.title}
+            </div>
+          )}
+        />
       </div>
     );
   }
