88c2eb2356c397b0d0c139887fa05bc82b99cf31
Vladimir Cirkovic <vladimir.cirkovic@relax-gaming.com>
2018-05-06 18:55:50 +0200

    begin final styling of card component

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { object } from "prop-types";
 import numeral from "numeral";
+import "./Card.css";
+
+let backdrop;
 
 class Card extends Component {
   render() {
     const poster = "https://image.tmdb.org/t/p/w500" + this.props.data.poster;
-    const backdrop =
-      "https://image.tmdb.org/t/p/original" + this.props.data.backdrop;
+    backdrop = "https://image.tmdb.org/t/p/original" + this.props.data.backdrop;
 
     const vote = this.props.data.vote
       ? this.props.data.vote > 0
@@ -21,12 +23,45 @@ class Card extends Component {
       : "-";
 
     return (
-      <div>
-        <h1>{this.props.data.original_title}</h1>
-        <h2>{vote}</h2>
-        <h2>{revenue}</h2>
+      <div className="col-xs-12 cardcont nopadding">
+        <div className="meta-data-container col-xs-12 col-md-8 push-md-4 col-lg-7 push-lg-5">
+          <h1>{this.props.data.original_title}</h1>
+          <span className="tagline">{this.props.data.tagline}</span>
+          <p>{this.props.data.overview}</p>
+          <div className="additional-details">
+            <div className="row nopadding release-details">
+              <div className="col-xs-6">
+                {" "}
+                Original Release:{" "}
+                <span className="meta-data">{this.props.data.release}</span>
+              </div>
+              <div className="col-xs-6">
+                {" "}
+                Running Time:{" "}
+                <span className="meta-data">
+                  {this.props.data.runtime} mins
+                </span>{" "}
+              </div>
+              <div className="col-xs-6">
+                {" "}
+                Box Office: <span className="meta-data">{revenue}</span>
+              </div>
+              <div className="col-xs-6">
+                {" "}
+                Vote Average: <span className="meta-data">{vote}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="poster-container nopadding col-xs-12 col-md-4 pull-md-8 col-lg-5 pull-lg-7 ">
+          <img id="postertest" className="poster" src={poster} />
+        </div>
       </div>
     );
+  }
+
+  componentDidUpdate() {
+    document.body.style.backgroundImage = "url(" + backdrop + ")";
   }
 }
 
